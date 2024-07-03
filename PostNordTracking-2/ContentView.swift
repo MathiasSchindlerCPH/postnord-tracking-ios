9d25d02d23bcf3a7cb0bd321686e04341e44a93a
MathiasSchindlerCPH <mathingvid@gmail.com>
2024-07-03 20:29:13 +0200

    Initial Commit

---
+++ PostNordTracking-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PostNordTracking-2
+//
+//  Created by Mathias Schindler on 03/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PostNordTracking-2/PostNordTracking_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PostNordTracking_2App.swift
+//  PostNordTracking-2
+//
+//  Created by Mathias Schindler on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PostNordTracking_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
