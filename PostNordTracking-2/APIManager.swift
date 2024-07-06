ba5a1b58cd2d755cec374e6996739d5b349bf756
MathiasSchindlerCPH <mathingvid@gmail.com>
2024-07-06 18:26:13 +0200

    Format shipment details as object before returning

--- PostNordTracking-2/APIManager.swift
+++ PostNordTracking-2/APIManager.swift
@@ -32,11 +32,37 @@ struct Location: Codable {
     let locationType: String?
 }
 
+struct ShipmentEvent: Identifiable {
+    let id = UUID()
+    let eventTime: String
+    let eventDescription: String
+    let locationName: String
+    let locationCountry: String
+    
+    init(eventTime: String, eventDescription: String, locationName: String, locationCountry: String) {
+        self.eventTime = ShipmentEvent.formatEventTime(eventTime)
+        self.eventDescription = eventDescription
+        self.locationName = locationName
+        self.locationCountry = locationCountry
+    }
+    
+    static func formatEventTime(_ eventTime: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        
+        if let date = formatter.date(from: eventTime) {
+            formatter.dateFormat = "yyyy.MM.dd HH:mm:ss"
+            return formatter.string(from: date)
+        } else {
+            return "Unknown time"
+        }
+    }
+}
 
 
 class APIManager {
-    static func fetchShipmentDetails(for id: String, locale: String, completion: @escaping (Result<[Event], Error>) -> Void) {
-        let apiKey = "b5dbbd1173510f2d9cad0f9f280ab330" // Replace with your actual API key
+    static func fetchShipmentDetails(for id: String, locale: String, completion: @escaping (Result<[ShipmentEvent], Error>) -> Void) {
+        let apiKey = "b5dbbd1173510f2d9cad0f9f280ab330" 
         let baseURL = "https://api2.postnord.com/rest/shipment/v5/trackandtrace/findByIdentifier.json"
         
         guard let url = URL(string: "\(baseURL)?apikey=\(apiKey)&id=\(id)&locale=\(locale)") else {
@@ -63,7 +89,16 @@ class APIManager {
                    let shipment = trackingResponse.shipments?.first,
                    let item = shipment.items?.first,
                    let events = item.events {
-                    completion(.success(events))
+                        var shipmentEvents = events.map { event in
+                            ShipmentEvent(
+                                eventTime: event.eventTime ?? "Unknown time",
+                                eventDescription: event.eventDescription ?? "Unknown time",
+                                locationName: event.location?.name ?? " ",
+                                locationCountry: event.location?.country ?? " "
+                            )
+                        }
+                    shipmentEvents.reverse()
+                    completion(.success(shipmentEvents))
                 } else {
                     completion(.failure(NSError(domain: "No shipments found", code: 1, userInfo: nil)))
                 }
